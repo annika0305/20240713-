553d27097de7b39d78bba2d7a30e570bde7d68cd
Annika <annika.liang@gmail.com>
2024-07-13 15:59:51 +0800

    sending email to multiple ppl

--- send_mail.py
+++ send_mail.py
@@ -5,19 +5,19 @@ from email.header import Header
 
 sender = "annika.liang@gmail.com"
 receiver = ["annica.liang@gmail.com","annika.liang@gmail.com"]
+for I in receiver:
+    msg = MIMEMultipart()
+    msg["From"] = sender
+    msg["To"] = I
+    header = Header("Test Send Email","utf-8")
+    msg["Subject"] = header.encode()
 
-msg = MIMEMultipart()
-msg["From"] = sender
-msg["To"] = receiver
-header = Header("Test Send Email","utf-8")
-msg["Subject"] = header.encode()
-
-body = "This is the email sent from python."
-msg.attach(MIMEText(body))
-#mbody = MIMEText(body)
-#msg.attach(mbody)
-ssl_context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com",465,context=ssl_context) as server:
-    server.login(sender,"txni jseq fcrr mpsx")
-    server.sendmail(sender,receiver,msg.as_string())
-print("Sending the Email Successfully")
+    body = "This is the email sent from python."
+    msg.attach(MIMEText(body))
+    #mbody = MIMEText(body)
+    #msg.attach(mbody)
+    ssl_context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com",465,context=ssl_context) as server:
+        server.login(sender,"txni jseq fcrr mpsx")
+        server.sendmail(sender,I,msg.as_string())
+    print("Sending the Email Successfully")
